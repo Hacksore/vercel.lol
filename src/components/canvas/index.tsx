86699419c3d4c331e3226344f125b70d0eb06f24
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-29 21:07:54 -0500

    ligma

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -3,14 +3,15 @@
 import React from "react";
 import { useEffect } from "react";
 
+const PUCK_SPEED = 0.8;
 export function Canvas() {
   const canvasRef = React.useRef < HTMLCanvasElement | null > (null);
   const ctxRef = React.useRef < CanvasRenderingContext2D | null > (null);
   const pos = React.useRef < { x: number; y: number; dx: number; dy: number } > ({
     x: 0,
     y: 0,
-    dx: 3,
-    dy: 3,
+    dx: 1,
+    dy: 1,
   });
 
   const CANVAS_WIDTH = 800;
@@ -45,24 +46,26 @@ export function Canvas() {
 
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    // Update the position
-    x += dx;
-    y += dy;
+    if (pos.current.y + TRIANGLE_HEIGHT >= canvasRef.current.height || pos.current.y < 0) {
+      pos.current.dy *= -1;
+    }
 
     // Reverse direction if hitting the canvas boundaries
-    if (x + TRIANGLE_WIDTH > canvasRef.current.width || x < 0) {
-      pos.current.dx = -dx;
-    }
-    if (y + TRIANGLE_HEIGHT > canvasRef.current.height || y < 0) {
-      pos.current.dy = -dy;
+    if (pos.current.x + TRIANGLE_WIDTH >= canvasRef.current.width || pos.current.x < 0) {
+      pos.current.dx *= -1;
     }
 
+    // Update the position
+    pos.current.x += pos.current.dx * PUCK_SPEED;
+    pos.current.y += pos.current.dy * PUCK_SPEED;
+
     // draw a solid triangle 
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + TRIANGLE_WIDTH, y);
-    ctx.lineTo(x + TRIANGLE_WIDTH / 2, y + TRIANGLE_HEIGHT);
-    ctx.fill();
+    ctx.fillRect(x, y, TRIANGLE_WIDTH, TRIANGLE_HEIGHT);
+    // ctx.beginPath();
+    // ctx.moveTo(pos.current.x, pos.current.y);
+    // ctx.lineTo(x + TRIANGLE_WIDTH, y);
+    // ctx.lineTo(x + TRIANGLE_WIDTH / 2, y + TRIANGLE_HEIGHT);
+    // ctx.fill();
 
     window.requestAnimationFrame(render);
   };
