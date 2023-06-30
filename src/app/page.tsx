989558ddf4107868e5f64e3a2b4484c4cfd15fdf
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-29 23:21:59 -0500

    Good looking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import { Canvas } from "../components/canvas";
+
 export default function Home() {
   return (
     <div>
       <Canvas />
+      <span className="fixed left-8 bottom-8 text-neutral-600 text-sm duration-300 hover:text-white">
+        <a target="_blank" href="https://github.com/Hacksore/vercel.lol">
+          Sauce
+        </a>
+      </span>
+      <span className="fixed bottom-8 left-1/2 -translate-x-1/2 text-neutral-600 text-sm">
+        © 2069 ▼ercel.lol
+      </span>
     </div>
   );
 }
