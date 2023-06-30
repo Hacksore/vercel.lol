07066531a9bdc711046b68f942fce8337797ee91
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-30 01:10:37 -0400

    title color swapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,31 @@ import { Canvas } from "../components/canvas";
 export default function Home() {
   return (
     <div>
-      <h1 className="fixed bg-clip-text text-transparent leading-5 bg-white bg-opacity-90 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-4xl md:text-8xl text-center">
-        <span>▲</span>
-        <br />
-        <span>The</span>
-        <br />
-        <span>Triangle</span>
-        <br />
-        <span className="tracking-widest">Company</span>
+      <h1 className="fixed bg-clip-text text-transparent bg-white bg-opacity-90 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-4xl md:text-8xl text-center">
+        <div id="t1" className="duration-300 pb-3 ">
+          ▲
+        </div>
+        <div id="t2" className="duration-300">
+          The
+        </div>
+        <div id="t3" className="duration-300 tracking-tight">
+          Triangle
+        </div>
+        {/* T4 Stack */}
+        <div id="t4" className="duration-300 tracking-widest">
+          Company
+        </div>
         <div
           id="subtitle"
-          className="rounded-full font-normal max-w-max mx-auto mt-8 text-xl p-[1.5px]"
+          className="rounded-full font-normal max-w-max mx-auto mt-8 text-sm md:text-xl p-[1px] md:p-[1.5px]"
         >
-          <div className="px-3 py-1 rounded-full bg-black bg-opacity-50 duration-300 text-white">
+          <div className="md:px-3 py-1 px-2 rounded-full bg-black bg-opacity-60 duration-300 text-white">
             We sell triangles.
           </div>
         </div>
       </h1>
       <Canvas />
-      <span className="fixed left-8 bottom-8 text-neutral-600 text-sm duration-300 hover:text-white">
+      <span className="fixed left-1/2 -translate-x-1/2 bottom-16 text-neutral-600 text-sm duration-300 hover:text-white">
         <a target="_blank" href="https://github.com/Hacksore/vercel.lol">
           Sauce
         </a>
