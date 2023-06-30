140c29438e75a7cd962defcca32df177e711c7e2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-29 21:19:45 -0500

    :ship:

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -3,16 +3,16 @@
 import React from "react";
 import { useEffect } from "react";
 
+// TODO: because refs were too hard we use globals
+let x = 0;
+let y = 0;
+let dx = 1;
+let dy = 1;
+
 const PUCK_SPEED = 0.8;
 export function Canvas() {
   const canvasRef = React.useRef < HTMLCanvasElement | null > (null);
   const ctxRef = React.useRef < CanvasRenderingContext2D | null > (null);
-  const pos = React.useRef < { x: number; y: number; dx: number; dy: number } > ({
-    x: 0,
-    y: 0,
-    dx: 1,
-    dy: 1,
-  });
 
   const CANVAS_WIDTH = 800;
   const CANVAS_HEIGHT = 600;
@@ -39,43 +39,41 @@ export function Canvas() {
       return;
     }
 
-    // make this bounce around the screen
-    ctx.fillStyle = "green";
-
-    let { x, y, dx, dy } = pos.current;
-
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    if (pos.current.y + TRIANGLE_HEIGHT >= canvasRef.current.height || pos.current.y < 0) {
-      pos.current.dy *= -1;
+    if (y + TRIANGLE_HEIGHT >= canvasRef.current.height || y < 0) {
+      dy *= -1;
     }
 
     // Reverse direction if hitting the canvas boundaries
-    if (pos.current.x + TRIANGLE_WIDTH >= canvasRef.current.width || pos.current.x < 0) {
-      pos.current.dx *= -1;
+    if (x + TRIANGLE_WIDTH >= canvasRef.current.width || x < 0) {
+      dx *= -1;
     }
 
     // Update the position
-    pos.current.x += pos.current.dx * PUCK_SPEED;
-    pos.current.y += pos.current.dy * PUCK_SPEED;
+    x += dx * PUCK_SPEED;
+    y += dy * PUCK_SPEED;
 
     // draw a solid triangle 
-    ctx.fillRect(x, y, TRIANGLE_WIDTH, TRIANGLE_HEIGHT);
-    // ctx.beginPath();
-    // ctx.moveTo(pos.current.x, pos.current.y);
-    // ctx.lineTo(x + TRIANGLE_WIDTH, y);
-    // ctx.lineTo(x + TRIANGLE_WIDTH / 2, y + TRIANGLE_HEIGHT);
-    // ctx.fill();
+    ctx.fillStyle = "#fff";
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + TRIANGLE_WIDTH, y);
+    ctx.lineTo(x + TRIANGLE_WIDTH / 2, y + TRIANGLE_HEIGHT);
+    ctx.fill();
 
     window.requestAnimationFrame(render);
   };
 
   return (
+  <>
     <canvas
       id="canvas"
-      style={{ background: "blue" }}
+      style={{ background: "#000" }}
       width="800"
       height="600"
     ></canvas>
+    </>
   );
 }
