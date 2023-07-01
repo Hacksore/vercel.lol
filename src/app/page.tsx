cf0aee49c5140976f6f49b322143f7dc048bbb50
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-07-01 11:22:36 +0200

    use doritos keyword

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           className="rounded-full bg-white bg-opacity-80 font-normal max-w-max mx-auto mt-8 text-sm md:text-xl p-[1px] md:p-[1.5px]"
         >
           <div className="px-3 py-1 rounded-full bg-black bg-opacity-60 duration-300 text-white">
-            We sell triangles.
+            We sell doritos.
           </div>
         </div>
       </h1>
